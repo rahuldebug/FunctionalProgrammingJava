8925725a5a473f9ee5417cb9322d09b94b8f6453
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-06-09 17:46:18 +0530

    Runnable lambda commit

---
+++ src/main/java/lambda/RunnableLambdaExample.java
@@ -0,0 +1,19 @@
+package lambda;
+
+public class RunnableLambdaExample {
+
+    public static void main(String[] args) {
+        Thread t1= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("old way to do stuff");
+            }
+        });
+        t1.run();
+        Runnable runnable= ()->{
+            System.out.println("do stuff in new way ");
+        };
+        Thread t2= new Thread(runnable);
+        t2.start();
+    }
+}
