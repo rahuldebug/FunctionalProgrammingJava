c9efb11232a82a305b04ccf1c2908944885d80f6
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2023-06-17 16:08:01 +0530

    sorting added

--- src/main/java/StreamApi/MapFlatMapExample.java
+++ src/main/java/StreamApi/MapFlatMapExample.java
@@ -3,12 +3,13 @@ package StreamApi;
 import data.Student;
 import data.StudentDatabase;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class MapFlatMapExample {
     // distinct are also being captured here
-    public static  List<String> getAllActivities(){
+    public static List<String> getAllActivities() {
         return StudentDatabase.getStudents()
                 .stream()
                 .map(Student::getActivities)
@@ -16,7 +17,8 @@ public class MapFlatMapExample {
                 .distinct()
                 .collect(Collectors.toList());
     }
-    public static int getAllActivitiesDistinctCount(){
+//count
+    public static int getAllActivitiesDistinctCount() {
         return (int) StudentDatabase.getStudents()
                 .stream()
                 .map(Student::getActivities)
@@ -24,9 +26,23 @@ public class MapFlatMapExample {
                 .distinct()
                 .count();
     }
+//sorting
+    public static List<Student> getSortedList() {
+        return StudentDatabase.getStudents().stream()
+                .sorted(Comparator.comparing(Student::getName))
+                .collect(Collectors.toList());
+    }
+    public static List<String> getSortedName() {
+        return StudentDatabase.getStudents().stream()
+                .map(Student::getName)
+                .sorted()
+                .collect(Collectors.toList());
+    }
 
     public static void main(String[] args) {
         System.out.println(getAllActivities());
         System.out.println(getAllActivitiesDistinctCount());
+        System.out.println(getSortedList());
+        System.out.println(getSortedName());
     }
 }
